a8672feb6c0895b32e711f8d1e92bd4a3eb09e32
Russell Gordon <rgordon@lcs.on.ca>
2025-03-01 12:28:59 -0500

    Initial Commit

---
+++ JokeFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokeFinder
+//
+//  Created by Russell Gordon on 2025-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Russell Gordon on 2025-03-01.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
