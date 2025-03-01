e668b1c35379f6fc8b8acf1fd617d66f79afd561
Russell Gordon <rgordon@lcs.on.ca>
2025-03-01 12:36:08 -0500

    Adjusted view model so that it does not use an extension to decode the JSON.

--- JokeFinder/ViewModels/JokeViewModel.swift
+++ JokeFinder/ViewModels/JokeViewModel.swift
@@ -30,12 +30,44 @@ class JokeViewModel {
     // won't "freeze up" while this function does it's job.
     func fetchJoke() async {
         
+        // 1. Attempt to create a URL from the address provided
+        let endpoint = "https://official-joke-api.appspot.com/random_joke"
+        guard let url = URL(string: endpoint) else {
+            debugPrint("Invalid address for JSON endpoint.")
+            return
+        }
+        
+        // 2. Fetch the raw data from the URL
+        //
+        // Network requests can potentially fail (throw errors) so
+        // we complete them within a do-catch block to report errors if they
+        // occur.
+        //
         do {
-            let url4 = URL(string: "https://official-joke-api.appspot.com/random_joke")!
-            let joke = try await URLSession.shared.decode(Joke.self, from: url4)
-            self.currentJoke = joke
+            
+            // Fetch the data
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // 3. Decode the data
+            
+            // Create a decoder object to do most of the work for us
+            let decoder = JSONDecoder()
+            
+            // Use the decoder object to convert the raw data into an instance of our Swift data type
+            let decodedData = try decoder.decode(Joke.self, from: data)
+            
+            // If we got here, decoding succeeded, return the instance of our data type
+            self.currentJoke = decodedData
+            
         } catch {
-            print("Download error: \(error.localizedDescription)")
+            
+            // Show an error that we wrote and understand
+            print("Count not retrieve data from endpoint, or could not decode into an instance of a Swift data type.")
+            print("----")
+            
+            // Show the detailed error to help with debugging
+            debugPrint(error)
+            
         }
         
     }
